ccb8ffaf5110cda79ba579435efaaf7fb86e7f7e
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-06-10 19:34:27 -0600

    creacion del documento Area

---
+++ modelos/Area.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'Conexion.php';
+
+class Area extends Conexion{
+    public $area_id;
+    public $area_nombre;
+    public $area_situacion;
+
+
+
+    public function __construct($args = [] )
+    {
+        $this->area_id = $args['area_id'] ?? null;
+        $this->area_nombre = $args['area_nombre'] ?? '';
+        $this->area_situacion = $args['area_situacion'] ?? '';
+    }
+
+    public function guardar(){
+        $sql = "INSERT INTO areas(area_nombre) values('$this->area_nombre')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function buscar(){
+        $sql = "SELECT * from areas where area_situacion = 1 ";
+
+        if($this->area_nombre != ''){
+            $sql .= " and area_nombre like '%$this->area_nombre%' ";
+        }
+
+        if($this->area_id != null){
+            $sql .= " and area_id = $this->area_id ";
+        }
+
+        $resultado = self::servir($sql);
+        return $resultado;
+    }
+
+    public function modificar(){
+        $sql = "UPDATE areastos SET area_nombre = '$this->area_nombre', area_sueldo = $this->area_sueldo where area_id = $this->area_id";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function eliminar(){
+        $sql = "UPDATE areastos SET area_situacion = 0 where area_id = $this->area_id";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+}
