eadbb76d6850d5e1ec125003e6f309c981e39e28
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-06-11 07:55:51 -0600

    creacion de archivo modificar para la carpeta empleados

--- vistas/empleados/modificar.php
+++ vistas/empleados/modificar.php
@@ -23,14 +23,42 @@ require '../../modelos/Empleado.php';
                 <input type="hidden" name="em_id" value="<?= $empleados[0]['EMPLEADO_ID'] ?>" >
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="empleado_nombre">Nombre del empleado</label>
-                        <input type="text" name="empleado_nombre" id="empleado_nombre" class="form-control" value="<?= $empleados[0]['empleado_NOMBRE'] ?>">
+                        <label for="emp_nombre">Nombre del empleado</label>
+                        <input type="text" name="emp_nombre" id="emp_nombre" class="form-control">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="empleado_precio">Precio del empleado</label>
-                        <input type="number" step="0.01" min="0" name="empleado_precio" id="empleado_precio" class="form-control" value="<?= $empleados[0]['empleado_PRECIO'] ?>">
+                        <label for="emp_dpi">DPI</label>
+                        <input type="number" name="emp_dpi" id="emp_dpi" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="emp_puesto">Puesto</label>
+                        <select name="emp_puesto" id="emp_puesto" class="form-control">
+                            <option value="">SELECCIONE...</option>
+                            <?php foreach ($empleados as $key => $empleado) : ?>
+                                <option value="<?= $empleado['EMP_ID'] ?>"><?= $empleado['EMP_PUESTO'] ?></option>
+                            <?php endforeach?>
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="emp_edad">Edad</label>
+                        <input type="number" name="emp_edad" id="emp_edad" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="emp_sexo">Sexo</label>
+                        <select name="emp_sexo" id="emp_sexo" class="form-control">
+                            <option value="">SELECCIONE...</option>
+                            <?php foreach ($clientes as $key => $cliente) : ?>
+                                <option value="<?= $cliente['SEX_ID'] ?>"><?= $cliente['SEX_DESCRIPCION'] ?></option>
+                            <?php endforeach?>
+                        </select>
                     </div>
                 </div>
                 <div class="row mb-3">
