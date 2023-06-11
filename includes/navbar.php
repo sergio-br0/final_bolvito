37a2da9c6f571d274c9911247cb453f5b97d4b12
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-06-11 06:45:49 -0600

    creacion del archivo navbar

---
+++ includes/navbar.php
@@ -0,0 +1,73 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">INGESOFT S.A.</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Empleados
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/empleados/index.php">Ingresar empleado</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/empleados/buscar.php">Buscar empleado</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/empleados/buscar.php">Organizacion Empresa</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Areas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/areas/index.php">Crear area</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/areas/buscar.php">Buscar area</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Puestos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/puestos/index.php">Crear puesto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/puestos/buscar.php">Buscar puesto</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Asignar Empleados
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/asignacion_area/index.php">Asignar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_bolvito/vistas/asignacion_area/buscar.php">Buscar</a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
